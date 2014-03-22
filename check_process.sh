20972cc78a7679243cd7cf45c91a714e690ff629
damarsan <damarsan2@gmail.com>
2014-03-22 20:14:59 +0100

    add iowait process info

--- check_process.sh
+++ check_process.sh
@@ -8,6 +8,7 @@ function check_threshold()
         current=$(echo "scale=0;100-$remaining * 100 / $total" | bc -l)
         process=$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 1 | awk -F' ' '{print $3}')
         process_memory=$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 1 | awk -F' ' '{print $1}')
+        process_iowait=$(iotop -oPa -n 2 -qqq | awk -F' ' '{print $12}' | head -n 1)
         if [ $current -gt $threshold ]
          then
                 counter=$((counter+1))
@@ -17,6 +18,6 @@ function check_threshold()
         check_threshold
         if [ "$counter" -eq 2 ]
         then
-                echo "on `date +'%d-%m-%Y %H:%M:%S'`. Total RAM utilization at ${current}% by ${process} with memory usage of ${process_memory}" |  mail -s "process info" \
-                "damarsan2@gmail.com"
+            echo "on `date +'%d-%m-%Y %H:%M:%S'`. RAM utilization at ${current}% by ${process} and iowait of ${process_iowait} with memory usage of ${process_memory}" |  mail -s "process info" \
+            "damarsan2@gmail.com"
          fi
