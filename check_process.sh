1b13504aa183432bf203fb35776d33de0f50a79f
David Martinez <damarsan2@gmail.com>
2014-03-28 16:15:40 +0100

    changes in script

--- check_process.sh
+++ check_process.sh
@@ -1,4 +1,5 @@
- find which process is eating 99% RAM
+#!/bin/bash
+# find which process is eating 99% RAM
 counter=0
 function check_threshold()
 {
@@ -6,9 +7,7 @@ function check_threshold()
         total=$(free | grep "Mem:" | awk '{print $2}')
         remaining=$(free | grep "Mem:" | awk '{print $4}')
         current=$(echo "scale=0;100-$remaining * 100 / $total" | bc -l)
-        process=$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 1 | awk -F' ' '{print $3}')
-        process_memory=$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 1 | awk -F' ' '{print $1}')
-        process_iowait=$(iotop -oPa -n 2 -qqq | awk -F' ' '{print $12}' | head -n 1)
+        process=$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 1 | awk -F' ' '{print $4}')
         if [ $current -gt $threshold ]
          then
                 counter=$((counter+1))
@@ -16,8 +15,11 @@ function check_threshold()
 }
 
         check_threshold
+	/bin/sleep 55
+        check_threshold
+
         if [ "$counter" -eq 2 ]
         then
-            echo "on `date +'%d-%m-%Y %H:%M:%S'`. RAM utilization at ${current}% by ${process} and iowait of ${process_iowait} with memory usage of ${process_memory}" |  mail -s "process info" \
+            echo "on `date +'%d-%m-%Y %H:%M:%S'`. RAM utilization at ${current}% by ${process}" |  mail -s "process info" \
             "damarsan2@gmail.com"
          fi
