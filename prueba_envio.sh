957ca46e19861c91639cdae72e203ceff6ba6ced
damarsan <damarsan2@gmail.com>
2014-05-01 13:59:55 +0200

    Update prueba_envio.sh
    fixed bug.

--- prueba_envio.sh
+++ prueba_envio.sh
@@ -5,4 +5,4 @@ FROM_ADDRESS=""
 SUBJECT="Prueba de envio"
 BODY="This is a linux mail system. Linux is one of the email operating systems which can be used to send and receive emails."
 
-echo ${BODY}| mail -s ${SUBJECT} ${TO_ADDRESS} -- -r ${FROM_ADDRESS}
+echo ${BODY} | mail -r ${FROM_ADDRESS} -s ${SUBJECT} ${TO_ADDRESS}
