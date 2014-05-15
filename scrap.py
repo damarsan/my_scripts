fe7e729df8310074ee78c20cd69359272dc907c1
David Martinez <damarsan2@gmail.com>
2014-05-15 13:12:30 +0200

    add jinja template and bugs solved

--- scrap.py
+++ scrap.py
@@ -3,6 +3,7 @@ import re
 import sys
 import dns.resolver
 import socket
+import requests
 from urlparse import urlparse
 
 ins_domains = open( sys.argv[1], "r" )
@@ -10,7 +11,7 @@ out = open("result_" + sys.argv[1], "w" )
 error = open("result_errors", "w")
 host =""
 for line in ins_domains:
-	
+	redir = ''	
 	try:
 		# GET HOSTNAME 
         	res = socket.gethostbyname(line.strip("\n"))
@@ -24,7 +25,17 @@ for line in ins_domains:
 		#GET TITLE PAGE
 		content = urllib2.urlopen('http://'+line).read()
 		content2 = urllib2.urlopen('http://'+line)
-		code = content2.getcode()
+		# STRIP TO REMOVE BLANK SPACES
+		r = requests.get('http://' + line.strip(), allow_redirects=False)
+		# GET HTTP REDIR STATUS
+		code = r.status_code
+		# GET URL REDIRECTION
+		getredir = content2.geturl()
+		getredir = getredir.split('/')
+
+		if len(getredir) == 4:
+			redir = getredir[3]
+		# GET TITLE PAGE
 		host = urlparse(content2.geturl()).netloc
 		allTitles =  re.compile('<title>(.*?)</title>')
 		title = re.findall(allTitles,content)
@@ -33,7 +44,7 @@ for line in ins_domains:
 		print host,x
 		continue
 	try:
-		out.write("%s,%s,%s,%s\n" % (host,res2[0],title[0],code))
+		out.write("%s,%s,%s,%s,%s\n" % (host,res2[0],title[0],code,"/"+redir))
 	except Exception as y:
 		out.write("%s,%s\n" % (host,y))
 
